1b65d6a1a76c9794f92968d4d4357c206303795f
David E. Perez Negron R. <david@nethunters.xyz>
2023-10-28 17:44:43 -0600

    Updating a String

--- Chapter8/strings/src/main.rs
+++ Chapter8/strings/src/main.rs
@@ -1,22 +1,34 @@
 fn main() {
-    let data = "initial contents";
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+    println!("s1 is {s1}");
 
-    let s = data.to_string();
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("s is {s}");
 
-    // the method also works on a literal directly:
-    let s = "initial contents".to_string();
+    // Concatenation with the + Operator or the format! Macro
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    
+    println!("s3 is {s3}");
+    
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
 
-    let s = String::from("initial contents");
+    let s = s1 + "-" + &s2 + "-" + &s3;
 
-    let hello = String::from("السلام عليكم");
-    let hello = String::from("Dobrý den");
-    let hello = String::from("Hello");
-    let hello = String::from("שָׁלוֹם");
-    let hello = String::from("नमस्ते");
-    let hello = String::from("こんにちは");
-    let hello = String::from("안녕하세요");
-    let hello = String::from("你好");
-    let hello = String::from("Olá");
-    let hello = String::from("Здравствуйте");
-    let hello = String::from("Hola");
+    println!("s is {s}");
+    
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+
+    println!("s is {s}");
 }
