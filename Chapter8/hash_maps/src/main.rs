d05b2024930875f5799a28489d339052ee419ee9
David E. Perez Negron R. <david@nethunters.xyz>
2023-10-28 21:03:42 -0600

    Updating a Hash Map

--- Chapter8/hash_maps/src/main.rs
+++ Chapter8/hash_maps/src/main.rs
@@ -1,11 +1,35 @@
 use std::collections::HashMap;
 
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+
+    // Overwriting a Value
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    // Adding a Key and Value Only If a Key Isn’t Present
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // Updating a Value Based on the Old Value
+    let text = "hello world wonderful world";
 
     let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    // field_name and field_value are invalid at this point, try using them and
-    // see what compiler error you get!
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+
 }
