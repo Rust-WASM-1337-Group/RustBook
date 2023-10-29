48927b6af352d94af0c3c620a266e064f738ab14
David E. Perez Negron R. <david@nethunters.xyz>
2023-10-28 19:33:29 -0600

    Accessing Values in a Hash Map

--- Chapter8/hash_maps/src/main.rs
+++ Chapter8/hash_maps/src/main.rs
@@ -6,4 +6,7 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
 
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
 }
