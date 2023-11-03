117ef0a3e9bbb841bef1c9cebf733a5e89f911b4
krabs <krabs@rustaceosbox>
2023-11-03 06:05:39 +0000

    mutable references 4

--- Chapter4/mutable_references/src/main.rs
+++ Chapter4/mutable_references/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-     let mut s = String::from("hello");
+    let mut s = String::from("hello");
 
-     {
-         let r1 = &mut s;
-     } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
 
-     let r2 = &mut s;
+    println!("{}, {}, and {}", r1, r2, r3);
 }
 
