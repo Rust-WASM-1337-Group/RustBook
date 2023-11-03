32e71d5e98ad7c07487fbac31cfc9623ac713174
krabs <krabs@rustaceosbox>
2023-11-03 06:06:52 +0000

    mutable references 5

--- Chapter4/mutable_references/src/main.rs
+++ Chapter4/mutable_references/src/main.rs
@@ -3,8 +3,10 @@ fn main() {
 
     let r1 = &s; // no problem
     let r2 = &s; // no problem
-    let r3 = &mut s; // BIG PROBLEM
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-    println!("{}, {}, and {}", r1, r2, r3);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
 
