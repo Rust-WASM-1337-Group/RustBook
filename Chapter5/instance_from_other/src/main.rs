01b1d95d0b831c0bec35de6d69c598f53d4def27
krabs <krabs@rustaceosbox>
2023-11-17 05:44:15 +0000

    Creating Instances from Other Instances with Struct Update Syntax

---
+++ Chapter5/instance_from_other/src/main.rs
@@ -0,0 +1,29 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    // --snip--
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+
+    let user3 = User {
+        email: String::from("another@example.com"),
+        ..user2
+    };
+}
