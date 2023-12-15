bbcc7db224a08abe36a009c5516eeeff3e3798db
krabs <krabs@rustaceosbox>
2023-12-15 05:32:08 +0000

    Example 2: Lucky Penny!

--- Chapter6/match_coins/src/main.rs
+++ Chapter6/match_coins/src/main.rs
@@ -7,11 +7,16 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
     }
 }
 
-fn main() {}
+fn main() { 
+   value_in_cents(Coin::Penny); 
+}
