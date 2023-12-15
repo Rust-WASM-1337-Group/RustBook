7d9e019c168db4a7572981c2496224819c998492
krabs <krabs@rustaceosbox>
2023-12-15 05:41:30 +0000

    Alaska Quarter

--- Chapter6/match_coins/src/main.rs
+++ Chapter6/match_coins/src/main.rs
@@ -1,22 +1,30 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
+
 fn main() { 
-   value_in_cents(Coin::Penny); 
+    let value = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("{}", value);
 }
