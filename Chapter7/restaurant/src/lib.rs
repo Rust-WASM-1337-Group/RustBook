3af3431d074528d782ced4e5be100364ee9c381e
krabs <krabs@rustaceosbox>
2024-01-12 04:36:45 +0000

    A use statement only applies in the scope it’s in

--- Chapter7/restaurant/src/lib.rs
+++ Chapter7/restaurant/src/lib.rs
@@ -6,6 +6,8 @@ mod front_of_house {
 
 use crate::front_of_house::hosting;
 
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
+mod customer {
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
 }
