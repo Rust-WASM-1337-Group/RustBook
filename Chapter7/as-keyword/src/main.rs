5af843304662f6930dfa8cc02e4920d050164d92
krabs <krabs@rustaceosbox>
2024-01-12 04:59:40 +0000

    Providing New Names with the as Keyword

---
+++ Chapter7/as-keyword/src/main.rs
@@ -0,0 +1,16 @@
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn function1() -> Result {
+    // --snip--
+    Ok(())
+}
+
+fn function2() -> IoResult<()> {
+    // --snip--
+    Ok(())
+}
+
+fn main() {
+
+}
