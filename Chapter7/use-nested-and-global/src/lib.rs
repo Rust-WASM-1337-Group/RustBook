74c2b2c50e99145317598baa8d3240b1753d0150
krabs <krabs@rustaceosbox>
2024-01-12 05:28:56 +0000

    Using nested and global

---
+++ Chapter7/use-nested-and-global/src/lib.rs
@@ -0,0 +1,12 @@
+// # NESTED
+// use std::cmp::Ordering;
+// use std::io;
+use std::{cmp::Ordering, io};
+
+// use std::io;
+// use std::io::Write;
+use std::io::{self, Write};
+
+// #GLOBAL
+// This use statement brings all public items defined in std::collections into the current scope.
+use std::collections::*;
