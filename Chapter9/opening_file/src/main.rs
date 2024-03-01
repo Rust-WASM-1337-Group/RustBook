9143ae0b5f866d5c271150485ffb3605a66630e6
krabs <krabs@rustaceosbox>
2024-03-01 04:59:03 +0000

    Listing 9-12: Changing main to return Result<(), E> allows the use of the ? operator on Result values

--- Chapter9/opening_file/src/main.rs
+++ Chapter9/opening_file/src/main.rs
@@ -1,13 +1,8 @@
-fn last_char_of_first_line(text: &str) -> Option<char> {
-    text.lines().next()?.chars().last()
-}
+use std::error::Error;
+use std::fs::File;
 
-fn main() {
-    assert_eq!(
-        last_char_of_first_line("Hello, world\nHow are you today?"),
-        Some('d')
-    );
+fn main() -> Result<(), Box<dyn Error>> {
+    let greeting_file = File::open("hello.txt")?;
 
-    assert_eq!(last_char_of_first_line(""), None);
-    assert_eq!(last_char_of_first_line("\nhi"), None);
+    Ok(())
 }
