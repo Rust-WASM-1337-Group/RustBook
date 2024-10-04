21f747d34c808099b0e9f1185c70f5c9f28c300f
krabs <krabs@rustaceosbox>
2024-10-04 05:54:37 +0000

    Thinking in Terms of Lifetimes

--- Chapter10/longest-string/src/main.rs
+++ Chapter10/longest-string/src/main.rs
@@ -4,14 +4,11 @@ fn main() {
     {
         let string2 = String::from("xyz");
         let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
     }
-    println!("The longest string is {result}");
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn longest<'a>(x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
 }
